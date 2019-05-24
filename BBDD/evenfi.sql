e41df103af06aa7ec1857f2411a47402836038a2
ALUMNADO\riluas <riluas@floridauniversitaria.es>
2019-05-24 09:00:22 +0200

    quitado acentos

--- BBDD/evenfi.sql
+++ BBDD/evenfi.sql
@@ -27,7 +27,10 @@ CREATE TABLE IF NOT EXISTS `audit_per` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla evenfi.audit_per: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `audit_per` DISABLE KEYS */;
+/*!40000 ALTER TABLE `audit_per` ENABLE KEYS */;
+
 -- Volcando estructura para tabla evenfi.comentario
 CREATE TABLE IF NOT EXISTS `comentario` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
@@ -42,9 +45,12 @@ CREATE TABLE IF NOT EXISTS `comentario` (
   CONSTRAINT `FK_comentario_comentario` FOREIGN KEY (`reply`) REFERENCES `comentario` (`id`),
   CONSTRAINT `FK_comentario_plan` FOREIGN KEY (`id_plan`) REFERENCES `plan` (`id`),
   CONSTRAINT `FK_comentario_usuario` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla evenfi.comentario: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
+
 -- Volcando estructura para tabla evenfi.compra
 CREATE TABLE IF NOT EXISTS `compra` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
@@ -55,9 +61,12 @@ CREATE TABLE IF NOT EXISTS `compra` (
   KEY `FK_compra_plan` (`plan`),
   CONSTRAINT `FK_compra_plan` FOREIGN KEY (`plan`) REFERENCES `plan` (`id`),
   CONSTRAINT `FK_compra_usuario` FOREIGN KEY (`user`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla evenfi.compra: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `compra` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compra` ENABLE KEYS */;
+
 -- Volcando estructura para tabla evenfi.linea
 CREATE TABLE IF NOT EXISTS `linea` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
@@ -69,9 +78,12 @@ CREATE TABLE IF NOT EXISTS `linea` (
   KEY `FK_linea_plan` (`plan`),
   CONSTRAINT `FK_linea_compra` FOREIGN KEY (`compra`) REFERENCES `compra` (`id`),
   CONSTRAINT `FK_linea_plan` FOREIGN KEY (`plan`) REFERENCES `plan` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla evenfi.linea: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `linea` DISABLE KEYS */;
+/*!40000 ALTER TABLE `linea` ENABLE KEYS */;
+
 -- Volcando estructura para tabla evenfi.plan
 CREATE TABLE IF NOT EXISTS `plan` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
@@ -80,14 +92,17 @@ CREATE TABLE IF NOT EXISTS `plan` (
   `estilo` varchar(255) NOT NULL,
   `detalles` varchar(255) NOT NULL,
   `n_compra` int(255) NOT NULL,
-  `localización` varchar(255) NOT NULL,
+  `localizacion` varchar(255) NOT NULL,
   `precio` int(255) NOT NULL,
   `fecha_in` date NOT NULL,
   `fecha_fin` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla evenfi.plan: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `plan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `plan` ENABLE KEYS */;
+
 -- Volcando estructura para tabla evenfi.usuario
 CREATE TABLE IF NOT EXISTS `usuario` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
@@ -97,14 +112,17 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `puntos` int(255) DEFAULT NULL,
   `tickets` varchar(255) DEFAULT NULL,
   `telf` int(255) NOT NULL,
-  `localización` varchar(255) NOT NULL,
-  `fecha_creación` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `localizacion` varchar(255) NOT NULL,
+  `fecha_creacion` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `tipo_ticket` varchar(255) NOT NULL,
   `estado` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla evenfi.usuario: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+
 -- Volcando estructura para disparador evenfi.usuario_before_delete
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
 DELIMITER //
